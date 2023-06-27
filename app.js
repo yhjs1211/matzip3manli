941202ea6daa2d2eb36e41699cf698de1acb92d0
yhjs1211 <yhjs1211@gmail.com>
2023-06-27 09:55:07 +0900

    validation update & app.js update

--- app.js
+++ app.js
@@ -14,12 +14,39 @@ const cookieParser = require('cookie-parser');
 const mysql = require('./database/db.js');
 const models = require('./database/Models/index.js');
 
+
 // request data middleware
 app.use(express.json());
 app.use(cookieParser());
 
-app.use('/',router);
+app.get('/',(req, res, next) => {
+   res.status(200).json({
+    path:{
+        user:{
+            1: 'POST /signup 회원가입',
+            2: 'POST /login 로그인',
+            3: 'GET /logout 로그아웃'
+        },
+        post:{
+            1: 'GET /posts 게시글 전체 조회',
+            2: 'GET /posts?zone= 지역별 조회',
+            3: 'GET /posts/:postId 게시글 상세 조회',
+            4: 'POST /posts 게시글 작성',
+            5: 'PUT /posts/:postId 게시글 수정',
+            6: 'DELETE /posts/:postId 게시글 삭제'
+        },
+        comment:{
+            1: 'GET /comments/:postId 게시글 상세 댓글 조회',
+            2: 'GET /comments/:userId 유저가 작성한 댓글 조회',
+            3: 'POST /comments/:postId 댓글 작성',
+            4: 'PUT /comments/:commentId 댓글 수정',
+            5: 'DELETE /comments/:commentId 댓글 삭제'
+        }
+    }
+   }) 
+});
 
+app.use('/',router);
 
 (async ()=>{
     await mysql.sync().then(()=>{
@@ -28,5 +55,3 @@ app.use('/',router);
         });
     });
 })();
-
-
