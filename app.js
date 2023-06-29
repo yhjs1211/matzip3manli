c91c4e9adbb24f9715259f50f481c5dfdd2fe3b7
kimseryung <eshika@naver.com>
2023-06-29 10:00:07 +0900

    pull request 안떠서 테스트
    테스트

--- app.js
+++ app.js
@@ -17,8 +17,6 @@ const models = require('./database/Models/index.js');
 // request data middleware
 app.use(express.json());
 app.use(cookieParser());
-
-// 만약 이미지가 없을 때 대체 이미지로 넣는 것
 app.use(express.static('static'));
 
 app.get('/', (req, res, next) => {
