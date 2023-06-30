33fe76b6974dfde4ca6da49154773d74f2136fbc
yhjs1211 <yhjs1211@gmail.com>
2023-07-01 03:23:41 +0900

    ver.1 Before Last

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 const variable = {
   db: {
-    host: 'express-database.cjx2t84z9jyl.ap-northeast-2.rds.amazonaws.com',
-    username: 'root',
-    password: '20211211',
-    database: 'matzip',
+    host: '',
+    username: '',
+    password: '',
+    database: '',
   },
   jwt: {
     secretKey: 'matzip-3manli',
